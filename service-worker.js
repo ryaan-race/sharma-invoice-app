eeddb0ac57ee3d0d877d4c4dc692a57df8570c70
ryaan-race <rajesh.vihangam@gmail.com>
2025-08-04 00:37:59 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -24,8 +24,6 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
-    })
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
