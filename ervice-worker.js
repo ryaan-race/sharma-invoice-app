c7ac4482a4dfb434850091dbb6e2378962dcb539
ryaan-race <rajesh.vihangam@gmail.com>
2025-08-03 23:06:25 +0530

    Add files via upload

---
+++ ervice-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'sharma-invoice-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  // You can add CDN URLs if desired:
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
